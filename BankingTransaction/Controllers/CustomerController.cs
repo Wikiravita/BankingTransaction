b29e2e1ed4ea6a0a19ef614568408bb41d0f945d
pubudu wiki <pubuduwikiravita@gmail.com>
2023-05-10 12:39:03 +0530

    Controller

--- BankingTransaction/Controllers/CustomerController.cs
+++ BankingTransaction/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using BankingTransaction.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankingTransaction.Controllers
 {
@@ -8,13 +9,53 @@ namespace BankingTransaction.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        public static Customer customer = new Customer();
-        private readonly IConfiguration _configuration;
+        private readonly DataContext _configuration;
 
-        public CustomerController(IConfiguration configuration)
+        public CustomerController(DataContext configuration)
         {
             _configuration = configuration;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> Get(int userId)
+        {
+            var customers = await _configuration.Customers
+                .Where(c => c.customerId == userId)
+                .Include(c => c.Account)
+                .Include(c => c.Transaction)
+                .ToListAsync();
+
+            return customers;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<List<Customer>>> CustomerRegister(UserRegDto request)
+        {
+            var customer = await _configuration.Customers.FindAsync(request.customerId);
+
+            if (customer == null)
+                return NotFound();
+
+            var newCustomer = new Customer
+            {
+                customerName = request.customerName,
+                customerNIC = request.customerNIC,
+                customerEmail = request.customerEmail,
+                customerPhone = request.customerPhone,
+                Password = request.Password
+
+            };
+
+            //CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            //user.Username = request.Username;
+            //user.PasswordHash = passwordHash;
+            //user.PasswordSalt = passwordSalt;
+
+            _configuration.Customers.Add(newCustomer);
+            await _configuration.SaveChangesAsync();
+
+            return await Get(newCustomer.customerId);
+        }
     }
 }
--- BankingTransaction/UserRegDto.cs
+++ BankingTransaction/UserRegDto.cs
@@ -1,29 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
-namespace BankingTransaction.Models
+namespace BankingTransaction
 {
-    [Table("Customer")]
-    public class Customer
+    public class UserRegDto
     {
-        [Key]
         public int customerId { get; set; }
         public string customerName { get; set; } = string.Empty;
-        [Required]
         public string customerEmail { get; set; } = string.Empty;
         public string customerPhone { get; set; } = string.Empty;
-        [Required]
         public string customerNIC { get; set; } = string.Empty;
-        [Required]
         public string DOB { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; }
         //public byte[] PasswordHash { get; set; }
         //public byte[] PaswordSalt { get; set; }
-        [JsonIgnore]
-        public Account Account { get; set; }
-        public Transaction Transaction { get; set; }
-      
     }
 }
